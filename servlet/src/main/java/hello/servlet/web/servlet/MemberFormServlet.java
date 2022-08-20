db2f9de9a59c905acc0d0684b4ba3490780fc3f2
Yerim lee <103623292+lyrWinterCat@users.noreply.github.com>
2022-08-20 17:38:30 +0900

    Spring MVC code

---
+++ servlet/src/main/java/hello/servlet/web/servlet/MemberFormServlet.java
@@ -0,0 +1,42 @@
+package hello.servlet.web.servlet;
+
+import hello.servlet.domain.member.MemberRepository;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "memberFormServlet",urlPatterns = "/servlet/members/new-form")
+public class MemberFormServlet extends HttpServlet {
+
+    private MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        //content 잡아주기
+        response.setContentType("text/html");
+        response.setCharacterEncoding("utf-8");
+
+        PrintWriter w = response.getWriter();
+        //webapp basic - hello-form과 같은 html 유형 >> java 코드로 썼을 뿐 => 작성 불편!!
+        w.write("<!DOCTYPE html>\n" +
+                "<html>\n" +
+                "<head>\n" +
+                " <meta charset=\"UTF-8\">\n" +
+                " <title>Title</title>\n" +
+                "</head>\n" +
+                "<body>\n" +
+                "<form action=\"/servlet/members/save\" method=\"post\">\n" +
+                " username: <input type=\"text\" name=\"username\" />\n" +
+                " age: <input type=\"text\" name=\"age\" />\n" +
+                " <button type=\"submit\">전송</button>\n" +
+                "</form>\n" +
+                "</body>\n" +
+                "</html>\n");
+    }
+}
