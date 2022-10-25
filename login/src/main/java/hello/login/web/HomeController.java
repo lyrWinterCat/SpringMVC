aa47968e1b096270274515d1c71d6e9873d3523c
Yerim lee <103623292+lyrWinterCat@users.noreply.github.com>
2022-10-25 16:44:25 +0900

    login MVC Complete

--- login/src/main/java/hello/login/web/HomeController.java
+++ login/src/main/java/hello/login/web/HomeController.java
@@ -78,7 +78,7 @@ public class HomeController {
         return "loginHome";
     }
 
-    @GetMapping("/")
+//    @GetMapping("/")
     public String homeLoginV3Spring
             (@SessionAttribute(name=SessionConst.LOGIN_MEMBER, required = false) Member loginMember, Model model){
 
@@ -93,4 +93,16 @@ public class HomeController {
         return "loginHome";
     }
 
+    @GetMapping("/")
+    public String homeLoginV3ArgumentResolver( @Login Member loginMember, Model model){
+
+        //세션에 회원 데이터가 없으면 home으로 이동
+        if(loginMember==null){
+            return "home";
+        }
+
+        //세션이 유지되면 로그인으로 이동
+        model.addAttribute("member",loginMember);
+        return "loginHome";
+    }
 }
